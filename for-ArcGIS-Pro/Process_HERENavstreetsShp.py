adf3d32507a6d7788b70c7973f00510c31b24b60
Melinda Morang <mmorang@esri.com>
2025-02-14 09:02:13 -0800

    Update copyright year

--- for-ArcGIS-Pro/Process_HERENavstreetsShp.py
+++ for-ArcGIS-Pro/Process_HERENavstreetsShp.py
@@ -1,6 +1,6 @@
 """Class to process raw HERE NAVSTREETS shapefile data into a network dataset.
 
-   Copyright 2024 Esri
+   Copyright 2025 Esri
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
